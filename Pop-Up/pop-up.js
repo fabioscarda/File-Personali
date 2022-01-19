a5959a14cabd4ea216dfca5d76031b8c6400b807
fabioscarda <95306459+fabioscarda@users.noreply.github.com>
2022-01-19 09:21:09 +0100

    Add files via upload

---
+++ Pop-Up/pop-up.js
@@ -0,0 +1,33 @@
+function pop_init( my_content ) {
+
+	var pop_height = "25%";
+	var pop_html = '<div class="pop-bg"></div><div class="pop-wrap"><p class="pop-x">X</p><div class="pop-content"></div></div>';
+
+	$("body").prepend( pop_html );
+
+	$(".pop-wrap").animate({ "height" : pop_height }, 250).click(function(event){
+		event.stopPropagation();
+	});
+
+	$(".pop-bg, .pop-x").bind("click", function() {
+		pop_close();
+	});
+
+	$(".pop-content").text(my_content);
+
+}
+
+function pop_close() {
+	$(".pop-bg, .pop-wrap").remove();
+	$("body").unbind("click");
+}
+
+$(document).ready(function() {
+
+	$(".popup-me").click(function(){
+
+		pop_init( "Questo è solo un esempio di popup!" );
+
+	});
+
+});
